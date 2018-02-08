a1b100b55fb01d3099f66d3161f40875c0b10460
Chewsterchew <chew@chew.pw>
2018-02-08 15:58:01 -0600

    Initial requiregems

---
+++ requiregems.rb
@@ -0,0 +1,14 @@
+begin
+  require 'cinch'
+rescue LoadError
+  puts "You're missing the gem `cinch`. Would you like to install this now? (y/n)"
+  input = gets.chomp
+  if input == 'y'
+    `gem install cinch`
+    puts 'Gem installed! Continuing..'
+  else
+    puts 'To continue, install the cinch gem'
+    exit
+  end
+end
+require 'yaml'
